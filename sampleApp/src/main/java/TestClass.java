95ae053f45afbcc7e0e73aff28b66dc4dad96400
René Bergelt <rene.bergelt@informatik.tu-chemnitz.de>
2018-03-28 13:10:21 +0200

    Added sample application

---
+++ sampleApp/src/main/java/TestClass.java
@@ -0,0 +1,12 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		if (de.renebergelt.test.Switches.DEBUG) {
+			System.out.println("DEBUG");
+		} else {
+			System.out.println("RELEASE");
+		}			
+	}
+	
+}
